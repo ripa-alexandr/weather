aaf08ab6a25d7ec8b30e7872f7eb212512d3a5ee
alexandr-ripa <alexandr.ripa@chisw.us>
2015-02-23 18:49:20 +0200

    Auto Mapper Configuration

--- Weather.Bootstrap/AutoMapperConfiguration.cs
+++ Weather.Bootstrap/AutoMapperConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿
 using AutoMapper;
 
+using Weather.Common.Dto;
 using Weather.Common.Entities;
 
 namespace Weather.Bootstrap
@@ -9,6 +10,13 @@ namespace Weather.Bootstrap
     {
         public static void Configure()
         {
+            ConfigureCommon();
+            ConfigureEntityToDto();
+            ConfigureDtoToEntity();
+        }
+
+        private static void ConfigureCommon()
+        {
             Mapper.CreateMap<WeatherDataEntity, WeatherDataEntity>()
                 .ForMember(d => d.Id, opt => opt.Ignore())
                 .ForMember(d => d.City, opt => opt.Ignore());
@@ -16,6 +24,28 @@ namespace Weather.Bootstrap
             Mapper.CreateMap<WeatherDescriptionEntity, WeatherDescriptionEntity>()
                 .ForMember(d => d.Id, opt => opt.Ignore())
                 .ForMember(d => d.WeatherData, opt => opt.Ignore());
+        }
+
+        private static void ConfigureEntityToDto()
+        {
+            Mapper.CreateMap<CityEntity, CityDto>();
+            Mapper.CreateMap<CountryEntity, CountryDto>();
+            Mapper.CreateMap<LinkEntity, LinkDto>();
+            Mapper.CreateMap<RegionEntity, RegionDto>();
+            Mapper.CreateMap<WeatherDataEntity, WeatherDataDto>();
+            Mapper.CreateMap<WeatherDescriptionEntity, WeatherDescriptionDto>();
+            Mapper.CreateMap<WorldEntity, WorldDto>();
+        }
+
+        private static void ConfigureDtoToEntity()
+        {
+            Mapper.CreateMap<CityDto, CityEntity>();
+            Mapper.CreateMap<CountryDto, CountryEntity>();
+            Mapper.CreateMap<LinkDto, LinkEntity>();
+            Mapper.CreateMap<RegionDto, RegionEntity>();
+            Mapper.CreateMap<WeatherDataDto, WeatherDataEntity>();
+            Mapper.CreateMap<WeatherDescriptionDto, WeatherDescriptionEntity>();
+            Mapper.CreateMap<WorldDto, WorldEntity>();
         }
     }
 }
