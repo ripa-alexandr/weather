5bb3e6d987c7b7752834b9bad78f080989a6abb4
alexandr.ripa <alexandr.ripa@chisw.com>
2015-04-09 13:02:55 +0300

    entity for website

---
+++ Weather.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Weather.Website.MvcApplication" Language="C#" %>
---
+++ Weather.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Weather.Website.Startup))]
+namespace Weather.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
