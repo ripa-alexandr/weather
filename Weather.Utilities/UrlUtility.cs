cc4c48eb28c97abe00eb601b3edae809055f9dff
alexandr-ripa <alexandr.ripa@chisw.us>
2015-06-04 12:59:02 +0300

    fix: sinoptic wrong combine url

--- Weather.Utilities/UrlUtility.cs
+++ Weather.Utilities/UrlUtility.cs
@@ -1,13 +1,14 @@
 ﻿
-using System;
-
 namespace Weather.Utilities
 {
     public static class UrlUtility
     {
-        public static string Combine(string baseUri, string relativeUri)
+        public static string Combine(string uri1, string uri2)
         {
-            return new Uri(new Uri(baseUri), relativeUri).ToString();
+            uri1 = uri1.TrimEnd('/');
+            uri2 = uri2.TrimStart('/');
+
+            return string.Format("{0}/{1}", uri1, uri2);
         }
     }
 }
