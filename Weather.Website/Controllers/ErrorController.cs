e3d8bce61d0eae1924a4294ebd736c40a8bfba7c
alexandr-ripa <alexandr.ripa@chisw.us>
2015-07-07 18:58:24 +0300

    http error handle

---
+++ Weather.Website/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Weather.Website.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Error404()
+        {
+            return View();
+        }
+
+        public ViewResult Error500()
+        {
+            return View();
+        }
+	}
+}
